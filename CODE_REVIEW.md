a14c4d3ac16bb771134d94023f3b89ed2e2f1ec3
JDevost <jdevost@coveo.com>
2019-01-15 09:56:50 -0500

    initial review

---
+++ CODE_REVIEW.md
@@ -0,0 +1,60 @@
+
+# Code Review
+
+## CoveoConstants
+
+I don't understand why `class LargeFileContainer` is in there. It doesn't have constants.
+
+
+## CoveoDocument
+
+isBase64() is duplicated in CoveoPush. Maybe use a Util class?
+
+in Validate(), rather than building a string for `error`, consider using an array and use `errors.append('some message')` and at the end, `' | '.join(errors)` (it will prevent trailings characters `|`)
+
+### SetDate
+
+The validation isn't right. It checks if not an empty string is passed when we expect a datetime object. I proposed we check if the instance is `datetime`, and in case a string is passed in, we try to make it a `datetime`.
+For example:
+```
+    def SetDate(self, p_Date: datetime):
+        """
+        SetDate.
+        Sets the date property.
+        :arg p_Date: datetime, set the date
+        """
+
+        self.logger.debug('SetDate')
+        # Check if string
+        if (type(p_Date) is str):
+            p_Date = datetime.fromisoformat(p_Date)
+
+        # Check we have a datetime object
+        if (type(p_Date) is not datetime):
+            Error(self, "SetDate: invalid date")
+
+        print('SetDate - trace 1 ' + p_Date.isoformat())
+        self.Date = p_Date.isoformat()
+```
+
+## CoveoPermissions
+
+We can reduce the number of functions we support.
+It's logical to only use arrays to add members, mappings, etc...
+We could remove these functions:
+* DocumentPermissionSet::AddAllowedPermission
+* DocumentPermissionSet::AddDeniedPermission
+* PermissionIdentityBody::AddMember
+* PermissionIdentityBody::AddMapping
+* PermissionIdentityBody::AddWellKnown
+* BatchPermissions::AddMember
+* BatchPermissions::AddMapping
+* BatchPermissions::AddDelete
+
+If you want to keep them, I would defer their implementation to their array counterpart. This way, it would be easier to maintain.
+For example:
+```
+  def AddMapping( self, p_PermissionIdentityBody: PermissionIdentityBody ):
+    self.AddMappings( [p_PermissionIdentityBody] )
+```
+
--- coveopush/CoveoDocument.py
+++ coveopush/CoveoDocument.py
@@ -19,7 +19,7 @@ from datetime import datetime
 #---------------------------------------------------------------------------------
 def isBase64(s):
   """
-  isBase64. 
+  isBase64.
   Checks if string is base64 encoded.
   Returns True/False
   """
@@ -31,8 +31,8 @@ def isBase64(s):
 #---------------------------------------------------------------------------------
 def Validate( obj ):
   """
-  Validate. 
-  Validates if all properties on the CoveoDocument are properly set. 
+  Validate.
+  Validates if all properties on the CoveoDocument are properly set.
   Returns True/False, Error
   """
   result = True
@@ -64,7 +64,7 @@ def Error(log, err):
 #---------------------------------------------------------------------------------
 class BatchDocument:
   """
-  class BatchDocument. 
+  class BatchDocument.
   Class to hold the Batch Document.
   """
   AddOrUpdate = []
@@ -73,7 +73,7 @@ class BatchDocument:
 #---------------------------------------------------------------------------------
 class DocumentToDelete:
   """
-  class DocumentToDelete. 
+  class DocumentToDelete.
   Class to hold the Document To Delete.
   It should consist of the DocumentId (URL) only."""
   #The unique document identifier for the source, must be the document URI.
@@ -98,7 +98,7 @@ class DocumentToDelete:
 #---------------------------------------------------------------------------------
 class Document:
   """
-  class Document. 
+  class Document.
   Class to hold the Document To Push.
   Mandatory properties: DocumentId (URL) and Title."""
   Data = ''
@@ -175,9 +175,9 @@ class Document:
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   def SetData( self, p_Data: str ):
     """
-    SetData. 
+    SetData.
     Sets the Data (plain text) property.
-    :arg p_Data: str, sets the Data (Plain Text) 
+    :arg p_Data: str, sets the Data (Plain Text)
     """
 
     self.logger.debug('SetData')
@@ -190,22 +190,28 @@ class Document:
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   def SetDate( self, p_Date: datetime ):
     """
-    SetDate. 
+    SetDate.
     Sets the date property.
     :arg p_Date: datetime, set the date
     """
 
     self.logger.debug('SetDate')
-    #Check if empty
-    if (p_Date==''):
-      Error(self, "SetDate: value not set")
+    # Check if string
+    if (type(p_Date) is str):
+      p_Date = datetime.fromisoformat(p_Date)
+
+    # Check we have a datetime object
+    if (type(p_Date) is not datetime):
+      Error(self, "SetDate: invalid date")
+
+    print('SetDate - trace 1 ' + p_Date.isoformat())
 
     self.Date = p_Date.isoformat()
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   def SetModifiedDate( self, p_Date: datetime ):
     """
-    SetModifiedDate. 
+    SetModifiedDate.
     Sets the ModifiedDate property.
     :arg p_Date: datetime, set the ModifiedDate date
     """
@@ -220,7 +226,7 @@ class Document:
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   def SetDateWithString( self, p_Date: str, p_Format: str ):
     """
-    SetDateWithString. 
+    SetDateWithString.
     Sets the Date property, based on the p_Format ("%Y-%m-%d") supplied.
     :arg p_Date: str, set the date
     :arg p_Format: str, set the format to use
@@ -236,7 +242,7 @@ class Document:
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   def SetCompressedEncodedData( self, p_CompressedEncodedData: str, p_CompressionType: CoveoConstants.Constants.CompressionType = CoveoConstants.Constants.CompressionType.ZLIB ):
     """
-    SetCompressedEncodedData. 
+    SetCompressedEncodedData.
     Sets the CompressedBinaryData property.
     Make sure to set the proper CompressionType and Base64 encode the CompressedEncodedData.
     :arg p_CompressedEncodedData: str, Encoded Data (base64 ecoded)
@@ -247,7 +253,7 @@ class Document:
     #Check if empty
     if (p_CompressedEncodedData==''):
       Error(self, "SetCompressedEncodedData: value not set")
-    
+
     #Check if base64 encoded
     if not (isBase64(p_CompressedEncodedData)):
       Error(self, "SetCompressedEncodedData: value must be base64 encoded.")
@@ -259,7 +265,7 @@ class Document:
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   def SetContentAndZLibCompress( self, p_Content: str ):
     """
-    SetContentAndCompress. 
+    SetContentAndCompress.
     Sets the CompressedBinaryData property, it will ZLIB compress the string and base64 encode it
     :arg p_Content: str, string
     """
@@ -269,7 +275,7 @@ class Document:
     if (p_Content==''):
       Error(self, "SetContentAndCompress: value not set")
 
-    compresseddata = zlib.compress(p_Content.encode('utf8'), zlib.Z_BEST_COMPRESSION) 
+    compresseddata = zlib.compress(p_Content.encode('utf8'), zlib.Z_BEST_COMPRESSION)
     encodeddata = base64.b64encode(compresseddata).decode('ascii')
 
 
@@ -280,7 +286,7 @@ class Document:
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   def GetFileAndCompress( self, p_FilePath: str ):
     """
-    GetFileAndCompress. 
+    GetFileAndCompress.
     Gets the file, compresses it (ZLIB), base64 encode it, set the filetype
     :arg p_FilePath: str, valid file
     """
@@ -297,7 +303,7 @@ class Document:
     with open(p_FilePath, mode='rb') as file: # b is important -> binary
       fileContent = file.read()
       compresseddata = zlib.compress(fileContent, zlib.Z_BEST_COMPRESSION)
-      encodeddata = base64.b64encode(compresseddata).decode('ascii') 
+      encodeddata = base64.b64encode(compresseddata).decode('ascii')
 
     #Get the extension
     __, file_extension = os.path.splitext(p_FilePath)
@@ -309,7 +315,7 @@ class Document:
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   def SetCompressedDataFileId( self, p_CompressedDataFileId: str):
     """
-    SetCompressedDataFileId. 
+    SetCompressedDataFileId.
     Sets the CompressedBinaryDataFileId property.
     :arg p_CompressedDataFileId: str, the fileId retrieved by the GetLargeFileContainer call
     """
@@ -326,7 +332,7 @@ class Document:
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   def AddMetadata(self, p_Key: str, p_Value: object):
     """
-    AddMetadata. 
+    AddMetadata.
     Sets the metadata.
     :arg p_Key: str, the key value to set
     :arg p_Value: object, the value or object to set (str or list)
@@ -337,7 +343,7 @@ class Document:
     if (p_Key==''):
       Error(self, "AddMetadata: key not set")
 
-    #Check if in reserved keys 
+    #Check if in reserved keys
     if (p_Key.lower() in [key.lower() for key in CoveoConstants.Constants.s_DocumentReservedKeys] ):
       Error(self, "AddMetadata: "+p_Key+ " is a reserved field and cannot be set as metadata.")
 
@@ -350,7 +356,7 @@ class Document:
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   def SetAllowedAndDeniedPermissions(self, p_AllowedPermissions: [], p_DeniedPermissions: [], p_AllowAnonymous: bool = False):
     """
-    SetAllowedAndDeniedPermissions. 
+    SetAllowedAndDeniedPermissions.
     Sets the permissions on the document.
     :arg p_AllowedPermissions: list of PermissionIdentities which have access
     :arg p_DeniedPermissions: list of PermissionIdentities which do NOT have access
@@ -368,7 +374,7 @@ class Document:
 
     simplePermissionSet = CoveoPermissions.DocumentPermissionSet('Set1')
     simplePermissionSet.AddAllowedPermissions( p_AllowedPermissions )
-    simplePermissionSet.AddDeniedPermissions( p_DeniedPermissions ) 
+    simplePermissionSet.AddDeniedPermissions( p_DeniedPermissions )
     simplePermissionSet.AllowAnonymous = p_AllowAnonymous
 
     simplePermissionLevel.AddPermissionSet(simplePermissionSet)
